93706b347107c710b878446fbceacf99279be96c
MEDISETTI VENKATA SAI <119730474+venkatasaimedisetti@users.noreply.github.com>
2023-01-03 20:43:17 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int ld=n%10;     while(n>0)     {         int x=n%10;         if(ld<x)         ld=x;         n=n/10;     }     printf("%d",ld); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -4,13 +4,12 @@ int main()
     int n;
     scanf("%d",&n);
     int ld=n%10;
-    while (n>0)
+    while(n>0)
     {
         int x=n%10;
-        if (ld<x)
+        if(ld<x)
         ld=x;
         n=n/10;
     }
     printf("%d",ld);
-    return 0;
 }
