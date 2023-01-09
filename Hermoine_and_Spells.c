56dd1cd3b97e9be27d4393b49bb4c4fecda52b7f
MEDISETTI VENKATA SAI <119730474+venkatasaimedisetti@users.noreply.github.com>
2023-01-09 20:49:46 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if (a+b>b+c && a+b>c+a)     printf("%d",a+b);     else if (b+c>a+b && b+c>c+a)     printf("%d",b+c);     else if (c+a>b+c && c+a>a+b)     printf("%d",c+a);     return 0; }

---
+++ Hermoine_and_Spells.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if (a+b>b+c && a+b>c+a) 
+    printf("%d",a+b);
+    else if (b+c>a+b && b+c>c+a) 
+    printf("%d",b+c);
+    else if (c+a>b+c && c+a>a+b) 
+    printf("%d",c+a);
+    return 0;
+}
